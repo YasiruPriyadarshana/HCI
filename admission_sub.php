188d19103b23fb2732540219dcaa9dd76c3b330f
juthee <SSCjju123@>
2020-10-16 19:30:26 +0530

    about yale

---
+++ admission_sub.php
@@ -0,0 +1,19 @@
+<?php
+    include_once './dbh.inc.php';
+
+
+    $fname = $_POST['fname'];
+    $lanme = $_POST['lanme'];
+    $email = $_POST['email'];
+    $bdate = $_POST['bdate'];
+
+
+
+
+$sql = "INSERT INTO admission (FirstName, LastName, Email, BirthDate)
+VALUES ( '$fname', '$lanme', '$email',$bdate)";
+
+mysqli_query($conn,$sql);
+
+$conn->close();
+?>
